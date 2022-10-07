e6dcb7826519a550e4dbc9eda5114b1ea93b8feb
TechPizza <23627133+TechPizzaDev@users.noreply.github.com>
2022-10-07 19:55:55 +0200

    Add project files.

---
+++ IndexIncremental.Cli/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.IO.Enumeration;
+
+namespace IndexIncremental.Cli;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        FileEnumerator enumerator = new("C:/Tmp/diffs/ComputeMesh", new EnumerationOptions()
+        {
+            RecurseSubdirectories = true,
+        });
+
+        while (enumerator.MoveNext())
+        {
+            var entry = enumerator.Current;
+        }
+
+        Console.ReadKey();
+    }
+}
+
+public sealed class FileEnumerator : FileSystemEnumerator<int>
+{
+    public FileEnumerator(string directory, EnumerationOptions? options = null) : base(directory, options)
+    {
+    }
+
+    protected override int TransformEntry(ref FileSystemEntry entry)
+    {
+        Console.WriteLine(entry.ToFullPath());
+
+        return default;
+    }
+}
